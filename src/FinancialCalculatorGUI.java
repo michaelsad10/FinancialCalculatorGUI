55203de3b7a65f48681a4b9443df84f689b2594a
michaelsad10 <32250042+michaelsad10@users.noreply.github.com>
2018-08-16 01:31:54 -0500

    Adding small stuff

--- src/FinancialCalculatorGUI.java
+++ src/FinancialCalculatorGUI.java
@@ -1,32 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.SwingUtilities;
+
+/*https://docs.oracle.com/javase/tutorial/uiswing/layout/visual.html
+  https://www.tutorialspoint.com/swing/swing_gridbaglayout.htm
+  https://www.tutorialspoint.com/swing/swing_gridbaglayout.htm
+  http://web.mit.edu/6.005/www/sp14/psets/ps4/java-6-tutorial/components.html
+  https://docs.oracle.com/javase/tutorial/uiswing/components/textfield.html
+  https://www.mortgagecalculator.org/
+  https://docs.oracle.com/javase/tutorial/uiswing/components/combobox.html
+ */
 
 public class FinancialCalculatorGUI {
-    JFrame f;
+
     FinancialCalculatorGUI(){
-        f = new JFrame("F"); // Makes the big frame
-        JTextArea ta = new JTextArea(200,200);
-        JPanel p1 = new JPanel();
-        JButton b1 = new JButton("Button 1");
-        JButton b2 = new JButton("Button 2");
-        JButton b3 = new JButton("Button 3");
-        p1.add(ta);
-        p1.add(b1);
-        JPanel p2 = new JPanel();
-        p2.add(ta);
-        p2.add(b2);
-        JPanel p3 = new JPanel();
-        p3.add(ta);
-        p3.add(b3);
-        JTabbedPane tb = new JTabbedPane();
-        tb.setBounds(50,50,200,200);
-        tb.add("what",p1);
-        tb.add("the", p2);
-        tb.add("f",p3);
-        f.add(tb);
-        f.setSize(400,400);
-        f.setLayout(new GridLayout(1,1));
-        f.setVisible(true);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new MainFrame("Hello");
+                frame.setSize(500,400);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+
+
 
 
     }
